d5d849e3a2b81213d4cee2f69f840f32ca7fb886
Kilian <kilian@kk>
2016-01-31 12:07:19 +0100

    missing class added

---
+++ src/etc/RecognizableWords.java
@@ -0,0 +1,7 @@
+package etc;
+
+public class RecognizableWords {
+
+    public static String[] greetings = {"hi", "hallo", "hey", "jo", "tag", "tach", "morgen", "moinsen", "servus", "yo", "goedendag"};
+
+}
